eca13c9925bc23f170fd5d8f063ca2ab73247e57
Rukartik <kartiknavgire.r12@gmail.com>
2022-02-28 16:37:59 +0530

    s

--- Elf_BaseFramework/src/test/java/skills/SampleTestCaseTest.java
+++ Elf_BaseFramework/src/test/java/skills/SampleTestCaseTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 import genericLibrary.Base_Class;
 
-public class SampleTestCase extends Base_Class{
+public class SampleTestCaseTest extends Base_Class{
 	
 	@Test
 	public void sample() {
